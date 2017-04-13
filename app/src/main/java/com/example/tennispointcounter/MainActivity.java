b35c1ce4fd46f0259e477a18e54a01dfd2f21dc6
elit1910 <walter.estlander@gmail.com>
2017-04-13 16:54:43 +0300

    improved version1.2

--- app/src/main/java/com/example/tennispointcounter/MainActivity.java
+++ app/src/main/java/com/example/tennispointcounter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import java.lang.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,18 +23,19 @@ public class MainActivity extends AppCompatActivity {
             "30",
             "40",
             "AD"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void setCheck() {
         if (setPlayer1 == 2 & setPlayer2 < 2) {
-            EditText edit_text1   = (EditText)findViewById(R.id.player1);
+            EditText edit_text1 = (EditText) findViewById(R.id.player1);
             displayScorePlayer1("Winner " + edit_text1.getText().toString());
-        }
-        else if (setPlayer2 == 2 & setPlayer1 < 2) {
-            EditText edit_text2   = (EditText)findViewById(R.id.player2);
+        } else if (setPlayer2 == 2 & setPlayer1 < 2) {
+            EditText edit_text2 = (EditText) findViewById(R.id.player2);
             displayScorePlayer2("Winner " + edit_text2.getText().toString());
         } else {
             if (GamesPlayer1 == 6 & GamesPlayer2 <= 4) {
@@ -58,47 +60,46 @@ public class MainActivity extends AppCompatActivity {
                     TieBreak = true;
                 }
             }
+        }
     }
-    }
+
     //Methods for application:
     public void GameCheck() {
         if (TieBreak) {
-            if(GameScorePlayer1==7 & GameScorePlayer2 <=5){
+            if (GameScorePlayer1 == 7 & GameScorePlayer2 <= 5) {
                 setPlayer1 = ++setPlayer1;
                 TieBreak = false;
-                GameScorePlayer1=0;
-                GameScorePlayer2=0;
-                GamesPlayer1=0;
-                GamesPlayer2=0;
-            }else if( GameScorePlayer2==7 & GameScorePlayer1 <=5){
+                GameScorePlayer1 = 0;
+                GameScorePlayer2 = 0;
+                GamesPlayer1 = 0;
+                GamesPlayer2 = 0;
+            } else if (GameScorePlayer2 == 7 & GameScorePlayer1 <= 5) {
                 setPlayer2 = ++setPlayer2;
                 TieBreak = false;
-                GameScorePlayer1=0;
-                GameScorePlayer2=0;
-                GamesPlayer1=0;
-                GamesPlayer2=0;}
-            else if (GameScorePlayer1>=6 & GameScorePlayer2>=6){
-                    if(GameScorePlayer1-GameScorePlayer2 ==2){
-                        setPlayer1 = ++setPlayer1;
-                        TieBreak=false;
-                        GameScorePlayer1=0;
-                        GameScorePlayer2=0;
-                        GamesPlayer1=0;
-                        GamesPlayer2=0;
-                    }
-                    else if(GameScorePlayer2-GameScorePlayer1 ==2){
-                        setPlayer2 = ++setPlayer2;
-                        TieBreak=false;
-                        GameScorePlayer1=0;
-                        GameScorePlayer2=0;
-                        GamesPlayer1=0;
-                        GamesPlayer2=0;
-                    }
+                GameScorePlayer1 = 0;
+                GameScorePlayer2 = 0;
+                GamesPlayer1 = 0;
+                GamesPlayer2 = 0;
+            } else if (GameScorePlayer1 >= 6 & GameScorePlayer2 >= 6) {
+                if (GameScorePlayer1 - GameScorePlayer2 == 2) {
+                    setPlayer1 = ++setPlayer1;
+                    TieBreak = false;
+                    GameScorePlayer1 = 0;
+                    GameScorePlayer2 = 0;
+                    GamesPlayer1 = 0;
+                    GamesPlayer2 = 0;
+                } else if (GameScorePlayer2 - GameScorePlayer1 == 2) {
+                    setPlayer2 = ++setPlayer2;
+                    TieBreak = false;
+                    GameScorePlayer1 = 0;
+                    GameScorePlayer2 = 0;
+                    GamesPlayer1 = 0;
+                    GamesPlayer2 = 0;
+                }
             }
             displayGamePlayer2TB(GameScorePlayer2);
             displayGamePlayer1TB(GameScorePlayer1);
-        }
-        else{
+        } else {
             if (GameScorePlayer2 == 4 & GameScorePlayer1 <= 2) {
                 GameScorePlayer1 = 0;
                 GameScorePlayer2 = 0;
@@ -129,54 +130,68 @@ public class MainActivity extends AppCompatActivity {
             displaySetPlayer1(setPlayer1);
         }
     }
+
     public void addOnePointPlayer1(View v) {
         GameScorePlayer1 = GameScorePlayer1 + 1;
         GameCheck();
         setCheck();
     }
+
     public void addOnePointPlayer2(View v) {
         GameScorePlayer2 = GameScorePlayer2 + 1;
         GameCheck();
         setCheck();
-         };
+    }
+
+    ;
+
     //Display methods
-    public void displaySetPlayer1(int score){
+    public void displaySetPlayer1(int score) {
         TextView scoreView = (TextView) findViewById(R.id.sets1);
-        scoreView.setText(String.valueOf(setPlayer1));}
-        //method for calculating the set score of Player2
+        scoreView.setText(String.valueOf(setPlayer1));
+    }
+
+    //method for calculating the set score of Player2
     public void displaySetPlayer2(int score) {
         TextView scoreView = (TextView) findViewById(R.id.sets2);
         scoreView.setText(String.valueOf(setPlayer2));
     }
+
     //method for calculating the score of the Game Player1
     public void displayGamePlayer1(int score) {
         TextView scoreView = (TextView) findViewById(R.id.Games1);
         scoreView.setText(String.valueOf(GamesPlayer1));
     }
+
     //method for calculating the score of the Game Player2
     public void displayGamePlayer2(int score) {
         TextView scoreView = (TextView) findViewById(R.id.Games2);
         scoreView.setText(String.valueOf(GamesPlayer2));
     }
+
     //method for calculating the score of the Game Player1
     public void displayScorePlayer1(String score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(score);
     }
+
     //method for calculating the score of the Game Player2
     public void displayScorePlayer2(String score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(score);
     }
+
     public void displayGamePlayer1TB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
     //method for calculating the score of the Game Player2
     public void displayGamePlayer2TB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+
     public void erase(View v) {
         GameScorePlayer1 = 0;
         GameScorePlayer2 = 0;
